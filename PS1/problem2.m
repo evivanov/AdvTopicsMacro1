e06d344d011051a930fe1c53cff8a786476eba03
Evgenii Ivanov <31668382+evivanov@users.noreply.github.com>
2021-11-04 21:56:07 +0100

    update for problem 2

--- PS1/problem2.m
+++ PS1/problem2.m
@@ -1,9 +1,9 @@
 clear all
 
 % Parameters
-alpha = 0.5;
-beta = 0.8;
-delta = 1;
+alpha = 0.3;
+beta = 0.97;
+delta = 0.01;
 
 z0 = 1;
 z1 = 2*z0;
@@ -15,8 +15,8 @@ T = 200;
 
 %%
 
-k = fsolve(@(x) F(x), k_star_new*ones(201,1));
-plot(1:T, k(1:T))
+k_sys = fsolve(@(x) F(x), k_star_new*ones(201,1));
+plot(1:T, k_sys(1:T))
 
 %%
 k = zeros(T,1);
@@ -66,61 +66,75 @@ plot(1:T, k(1:T))
 
 %%
 
-% k = zeros(T,1);
-% k(T) = k_star_new;
-% c = zeros(T,1);
-% 
-% lower_bound_guess_k_t_1 = k_star;
-% upper_bound_guess_k_t_1 = k(T);
-% %z1*k(1)^alpha + (1-delta) * k(1);
-% k(T-1) = k_star_new;%(lower_bound_guess_k_t_1+upper_bound_guess_k_t_1)/2;
-% 
-% c(T) = z1*k(T)^alpha + (1-delta) * k(T) - k(T);
-% c(T-1) = (1/beta)*c(T)/( z1*alpha*k(T)^(alpha-1) + 1 - delta);
-% 
-% 
-% ki = k(T-1);
-% ki1 = 0;
-% 
-% while abs(lower_bound_guess_k_t_1 - upper_bound_guess_k_t_1) > 1e-7
-%     ki = k(T-1);
-%     k(1:T-2) = zeros(T-3+1,1);
-%     c(1:T-2) = zeros(T-3+1,1);
-%     for t = T-2:-1:1
-%        c(t) = (1/beta)*c(t+1)/( z1*alpha*k(t+1)^(alpha-1) + 1 - delta);
-%        func = @(x) (k(t+1) - z1*x.^alpha - (1-delta).*x + c(t));
-%        k(t) = lsqnonlin(func, k(t+1), lower_bound_guess_k_t_1, upper_bound_guess_k_t_1);
-%        if k(t) < k_star
+k = zeros(T,1);
+k(T) = k_star_new;
+c = zeros(T,1);
+
+lower_bound_guess_k_t_1 = k_star;
+upper_bound_guess_k_t_1 = k(T);
+%z1*k(1)^alpha + (1-delta) * k(1);
+k(T-1) = k_star_new;%(lower_bound_guess_k_t_1+upper_bound_guess_k_t_1)/2;
+
+c(T) = z1*k(T)^alpha + (1-delta) * k(T) - k(T);
+c(T-1) = (1/beta)*c(T)/( z1*alpha*k(T)^(alpha-1) + 1 - delta);
+
+
+ki = k(T-1);
+ki1 = 0;
+
+while abs(lower_bound_guess_k_t_1 - upper_bound_guess_k_t_1) > 1e-3
+    ki = k(T-1);
+    k(1:T-2) = zeros(T-3+1,1);
+    c(1:T-2) = zeros(T-3+1,1);
+    for t = T-2:-1:1
+        c(t) = z1*k(t)^alpha + (1-delta) * k(t) - k(t+1);
+        if c(t) < 0
+            break;
+        end
+        func = @(x) 1/(z1*x^alpha + (1-delta)*x - k(t+1)) - beta*(z1*alpha*k(t+1)^(alpha-1)/(z1*(k(t+1))^alpha + (1-delta)*k(t+1) - k(t+2)));
+        k(t) = lsqnonlin(func, k_star_new, k_star, k_star_new);
+        if k(t) < k_star
+           lower_bound_guess_k_t_1 = ki;
+           break;
+       end
+       if k(t) > k_star_new
+           upper_bound_guess_k_t_1 = ki;
+           break;
+       end
+       if (k(t) > k(t+1))
+           lower_bound_guess_k_t_1 = ki;
+           break;
+       end
+    end
+%     if (k(1) < k_star && k(1) > 0)
 %            lower_bound_guess_k_t_1 = ki;
-%            break;
-%        end
-%        if k(t) > k_star_new
-%            upper_bound_guess_k_t_1 = ki;
-%            break;
-%        end
-% %        if (k(t) > k(t+1))
-% %            lower_bound_guess_k_t_1 = ki;
-% %            break;
-% %        end
 %     end
-% %     if (k(1) < k_star && k(1) > 0)
-% %            lower_bound_guess_k_t_1 = ki;
-% %     end
-% %     if (k(1) > k_star_new)
-% %            upper_bound_guess_k_t_1 = ki;
-% %     end
-%     ki1 = (lower_bound_guess_k_t_1+upper_bound_guess_k_t_1)/2;
-%     k(T-1) = ki1;
-% end
-% 
-% 
-% 
-% plot(1:T, k(1:T))
-% 
+%     if (k(1) > k_star_new)
+%            upper_bound_guess_k_t_1 = ki;
+%     end
+    ki1 = (lower_bound_guess_k_t_1+upper_bound_guess_k_t_1)/2;
+    k(T-1) = ki1;
+end
+
+
+
+plot(1:T, k(1:T))
+
 
 %%
 
-N = 1000;
+alpha = 0.3;
+beta = 0.9;
+delta = 0.01;
+
+z0 = 1;
+z1 = 2*z0;
+
+k_star = ( 1/(alpha*z0) * ( 1/beta - 1 + delta ) )^(1/(alpha-1));
+k_star_new = ( 1/(alpha*z1) * ( 1/beta - 1 + delta ) )^(1/(alpha-1));
+
+
+N = 500;
 k_grid = linspace(k_star, k_star_new, N);
 
 k = zeros(T+1,1);
@@ -143,7 +157,9 @@ for i = N:-1:1
         func = @(x) 1/(z1*x^alpha + (1-delta)*x - k(t+1)) - beta*(z1*alpha*k(t+1)^(alpha-1)/(z1*(k(t+1))^alpha + (1-delta)*k(t+1) - k(t+2)));
         k(t) = lsqnonlin(func, k_star_new, k_star, k_star_new);
     end
-    if abs(k(1) - k_star) < 1e-2
+    if abs(k(1) - k_star) < 1e-5
         break
     end
 end
+
+plot(1:T, k(1:T))
