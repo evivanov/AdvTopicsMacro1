1da43b72ef9f470b177110dcce0d7b9d5e52df11
Evgenii Ivanov <31668382+evivanov@users.noreply.github.com>
2021-11-05 10:50:35 +0100

    bye Euler

--- PS1/euler_equation.m
+++
@@ -1,6 +0,0 @@
-function value = euler_equation(k_pr, k, alpha, beta, delta)
-% The Euler equation from which we have to find the roots (in terms of
-% k_pr):
-% test
-value = (k^alpha - (1-delta)*k - k_pr)^(-1) - beta*(1 + alpha*k_pr^(alpha-1) - delta) * (k_pr^alpha - (1-delta)*k_pr - k_pr)^(-1);
-end
